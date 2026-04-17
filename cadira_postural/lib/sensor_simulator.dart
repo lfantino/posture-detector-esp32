b917c2ff8a18f940c6aeba9941a502d7025123b4
martinavinolas <vinolasplamartina@gmail.com>
2026-04-17 21:41:03 +0200

    Afegit simulador de sensors

---
+++ cadira_postural/lib/sensor_simulator.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:math';
+
+class SensorSimulator {
+  final Random _random = Random();
+  StreamController<List<double>>? _controller;
+  Timer? _timer;
+
+  Stream<List<double>> get stream => _controller!.stream;
+
+  void start() {
+    _controller = StreamController<List<double>>.broadcast();
+    _timer = Timer.periodic(const Duration(milliseconds: 500), (_) {
+      _controller!.add(_generateList());
+    });
+  }
+
+  void stop() {
+    _timer?.cancel();
+    _controller?.close();
+  }
+
+  List<double> _generateList() {
+    return List.generate(11, (_) => _random.nextDouble() * 100);
+  }
+}
