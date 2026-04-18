6f54e504a886d4a8736fd943414e10a16e44a767
lfantino <lucia.fantino@gmail.com>
2026-04-18 19:44:45 +0200

    main.ino corregido

--- Firmware/main.ino
+++ Firmware/main.ino
@@ -1,9 +1,7 @@
-#include <Arduino.h>
-
 // Pines Ultrasonidos
-const int trigPins[] =  23;
+const int trigPins[] = {23, 23, 23};
 const int echoPins[] = {16, 17, 33};
-const char* usLabels[] = {"Cervical", "Dorsal", "Lumbar"};
+const char *usLabels[] = {"Cervical", "Dorsal", "Lumbar"};
 
 // Pines Multiplexor
 const int muxCom = 32;
@@ -11,9 +9,9 @@ const int sPins[] = {18, 19, 21, 22}; // S0=32, S1=33, S2=18, S3=19
 
 void setup() {
   Serial.begin(115200);
-  
+
   // Setup Ultrasonidos
-  for(int i=0; i<3; i++) {
+  for (int i = 0; i < 3; i++) {
     pinMode(trigPins[i], OUTPUT);
     pinMode(echoPins[i], INPUT);
   }
@@ -22,43 +20,49 @@ void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(sPins[i], OUTPUT);
   }
-  
+
   Serial.println("--- SISTEMA DE POSTURA COMPLETO ---");
 }
 
 void loop() {
   bool ocupado = false;
   int lecturasFSR[12];
-1
+
   // 1. Leer los 12 FSR vía MUX primero para decidir el sampling
   for (int i = 0; i < 12; i++) {
     for (int j = 0; j < 4; j++) {
       digitalWrite(sPins[j], (i >> j) & 0x01);
     }
-    delay(2); 
+    delay(2);
     lecturasFSR[i] = analogRead(muxCom);
     // Si algún sensor del asiento (canales 6-11) supera un umbral, está ocupado
-    if(i >= 6 && lecturasFSR[i] > 500) ocupado = true; 
+    if (i >= 6 && lecturasFSR[i] > 500)
+      ocupado = true;
   }
 
   // 2. Si está ocupado, leemos ultrasonidos y enviamos todo
   if (ocupado) {
-    for(int i=0; i<3; i++) {
-      digitalWrite(trigPins[i], LOW); delayMicroseconds(2); // Limpia pin
-      digitalWrite(trigPins[i], HIGH); delayMicroseconds(10); // Manda pulso de activación
+    for (int i = 0; i < 3; i++) {
+      digitalWrite(trigPins[i], LOW);
+      delayMicroseconds(2); // Limpia pin
+      digitalWrite(trigPins[i], HIGH);
+      delayMicroseconds(10);          // Manda pulso de activación
       digitalWrite(trigPins[i], LOW); // Corta pulso
-      long duration = pulseIn(echoPins[i], HIGH); // Mide cuánto tiempo tarda en llegar el sonido y activar el echoPin
+      long duration =
+          pulseIn(echoPins[i], HIGH); // Mide cuánto tiempo tarda en llegar el
+                                      // sonido y activar el echoPin
       float distancia = duration * 0.034 / 2;
-      Serial.print(distancia); Serial.print(",");
+      Serial.print(distancia);
+      Serial.print(",");
     }
-    for(int i=0; i<12; i++) {
+    for (int i = 0; i < 12; i++) {
       Serial.print(lecturasFSR[i]);
-      if(i < 11) Serial.print(",");
+      if (i < 11)
+        Serial.print(",");
     }
     Serial.println();
     delay(500); // Sampling rápido (Modo Activo)
-  } 
-  else {
+  } else {
     Serial.println("Silla vacía - Modo Ahorro");
     delay(10000); // Sampling lento (Modo Reposo)
   }
